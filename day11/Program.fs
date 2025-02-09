bef404663ebe697b727cf9715bbf4d57fa362924
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2025-02-09 19:38:37 +0100

    refactor: enhance blink and part functions with XML documentation and improve variable naming for clarity

--- day11/Program.fs
+++ day11/Program.fs
@@ -1,69 +1,89 @@
 ﻿open Xunit
 open FsUnit.Xunit
+open System.Diagnostics
 
 
-let blink stone = 
-    if stone = 0L then
+
+/// <summary>
+/// Transforms a stone number according to specific rules:
+/// - If stone is 0, returns [1]
+/// - If stone's digit length is even, splits it into two equal parts
+/// - If stone's digit length is odd, multiplies it by 2024
+/// </summary>
+/// <param name="stoneNumber">The input stone number to transform</param>
+/// <returns>List of transformed stone numbers</returns>
+let blink stoneNumber = 
+    if stoneNumber = 0L then
         [ 1L ]
     else 
-        let s = stone.ToString()
+        let numberAsString = stoneNumber.ToString()
 
-        if s.Length % 2 = 0 then
-            [int64 s[.. (s.Length / 2 - 1)]; int64 s[s.Length / 2 ..]]
+        if numberAsString.Length % 2 = 0 then
+            let midPoint = numberAsString.Length / 2
+            [int64 numberAsString[.. (midPoint - 1)]; int64 numberAsString[midPoint ..]]
         else 
-            [ stone * 2024L ]
+            [ stoneNumber * 2024L ]
 
 
 
-let part1 (stones: int64 seq) =
-    (List.ofSeq stones, [ 1..25 ])
-    ||> List.fold (fun stones _ -> stones |> List.collect blink )
+/// <summary>
+/// Processes stones for 25 iterations using the transformation rules
+/// </summary>
+/// <param name="initialStones">Initial sequence of stone numbers</param>
+/// <returns>Total count of stones after 25 iterations</returns>
+let part1 (initialStones: int64 seq) =
+    (List.ofSeq initialStones, [ 1..25 ])
+    ||> List.fold (fun accumulatedStones _ -> accumulatedStones |> List.collect blink)
     |> List.length
 
 
 
-let part2 (stones:int64 seq) =
-    let stones = stones |> Seq.map (fun x -> (x, 1L)) |> Map.ofSeq
+/// <summary>
+/// Processes stones for 75 iterations while tracking stone count frequencies
+/// </summary>
+/// <param name="initialStones">Initial sequence of stone numbers</param>
+/// <returns>Sum of all stone frequencies after 75 iterations</returns>
+let part2 (initialStones: int64 seq) =
+    let stoneFrequencies = initialStones |> Seq.map (fun x -> (x, 1L)) |> Map.ofSeq
 
-    (stones, [ 1..75 ])
-    ||> List.fold (fun stones _ -> 
-        (Map.empty, stones)
-        ||> Map.fold (fun acc key value -> 
-            (acc, blink key)
-            ||> List.fold (fun acc newKey -> 
-                let newValue = value + (acc |> Map.tryFind newKey |> Option.defaultValue 0L)
-                acc |> Map.add newKey newValue)))
+    (stoneFrequencies, [ 1..75 ])
+    ||> List.fold (fun currentFrequencies _ -> 
+        (Map.empty, currentFrequencies)
+        ||> Map.fold (fun newFrequencies originalStone frequency -> 
+            (newFrequencies, blink originalStone)
+            ||> List.fold (fun updatedFrequencies transformedStone -> 
+                let newFrequency = frequency + (updatedFrequencies |> Map.tryFind transformedStone |> Option.defaultValue 0L)
+                updatedFrequencies |> Map.add transformedStone newFrequency)))
     |> Map.values
     |> Seq.sum
 
+/// <summary>
+/// Parses space-separated string input into a sequence of int64 numbers
+/// </summary>
+/// <param name="inputText">Space-separated string of numbers</param>
+/// <returns>Sequence of parsed int64 numbers</returns>
+let parse (inputText: string) = 
+    inputText.Split(" ") |> Seq.map int64
 
-
-let parse (input: string) = 
-    input.Split(" ") |> Seq.map int64
-
-
+// Test module
 module Example =
-    let input = "125 17"
+    let sampleInput = "125 17"
 
     [<Fact>]
     let testPart1 () =
-        parse input |> part1 |> should equal 55312
-
-    
-
-open System.Diagnostics
+        parse sampleInput |> part1 |> should equal 55312
 
 [<EntryPoint>]
 let main _ = 
-    let input = stdin.ReadToEnd().TrimEnd()
-    let stones = parse input
+    let inputText = stdin.ReadToEnd().TrimEnd()
+    let initialStones = parse inputText
 
-    let stopwatch = Stopwatch.StartNew()
+    let executionTimer = Stopwatch.StartNew()
 
-    stones |> part1 |> printfn "Part 1: %d"
-    stones |> part2 |> printfn "Part 2: %d"
+    initialStones |> part1 |> printfn "Part 1: %d"
+    initialStones |> part2 |> printfn "Part 2: %d"
 
-    stopwatch.Stop()
-    printfn $"Elapsed time: %.4f{stopwatch.Elapsed.TotalSeconds} seconds"
+    executionTimer.Stop()
+    printfn $"Elapsed time: %.4f{executionTimer.Elapsed.TotalSeconds} seconds"
 
     0
