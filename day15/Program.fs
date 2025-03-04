b634185fbc6d17a61e1d885548551597f021f0e9
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2025-03-04 20:39:52 +0100

    feat: implement Part2 module for enhanced robot movement simulation and map handling

--- day15/Program.fs
+++ day15/Program.fs
@@ -21,6 +21,7 @@ let inline swap (i, j) (a: 'T[]) =
     let temp = a[i]
     a[i] <- a[j]
     a[j] <- temp
+    a
 
 
 let moveWithBox (map: Cell[][]) (ri: int) (rj: int) (di: int) (dj: int) =
@@ -105,6 +106,193 @@ let part1 ((map: Cell[][], moves: Dir[])) =
     sum
 
 
+module Part2 =
+    type Cell2 =
+        | Robot
+        | BoxL
+        | BoxR
+        | Wall
+        | Empty
+
+    let downgrade =
+        function
+        | Robot -> Cell.Robot
+        | BoxL
+        | BoxR
+        | Wall -> Cell.Wall
+        | Empty -> Cell.Empty
+
+
+    let toString (map: Cell2[][]) =
+        map
+        |> Array.map (fun row ->
+            row
+            |> Array.map (function
+                | Robot -> '@'
+                | BoxL -> 'O'
+                | BoxR -> 'O'
+                | Wall -> '#'
+                | Empty -> '.')
+            |> System.String)
+        |> String.concat "\n"
+
+
+
+    let findRobot (map: Cell2[][]) =
+        map |> Array.map (fun row -> row |> Array.map downgrade) |> findRobot
+
+    let rec pushLeft (i, j) (map: Cell2[][]) =
+        assert (map[i][j] = BoxR)
+        if j < 2 || map[i][j - 2] <> BoxL then
+            None // Can't push because no BoxL in the expected position
+        else
+
+        match map[i][j - 2] with
+        | Wall -> None
+        | Empty ->
+            // #..[].. -> #.[]...
+            let newRow = map[i] |> swap (j - 2, j - 1) |> swap (j - 1, j)
+            map |> Array.updateAt i newRow |> Some
+        | BoxR ->
+            pushLeft (i, j - 2) map
+            |> Option.map (fun map ->
+                assert (map[i][j - 2] = Empty)
+                //delegate
+                pushLeft (i, j) map |> Option.get)
+        | c -> failwithf $"%A{c} !?"
+
+
+
+    let rec pushUp (i, j) (map: Cell2[][]) =
+        match map[i][j] with
+        | BoxL ->
+            assert (map[i][j + 1] = BoxR)
+
+            match map[i - 1][j], map[i - 1][j + 1] with
+            | Wall, _
+            | _, Wall -> None
+            | Empty, Empty ->
+                let newRow1 = map[i - 1] |> Array.updateAt j BoxL |> Array.updateAt (j + 1) BoxR
+                let newRow2 = map[i] |> Array.updateAt j Empty |> Array.updateAt (j + 1) Empty
+                map |> Array.updateAt (i - 1) newRow1 |> Array.updateAt i newRow2 |> Some
+            // []
+            | BoxL, BoxR
+            // ].
+            | BoxR, Empty ->
+                pushUp (i - 1, j) map
+                |> Option.map (fun map ->
+                    assert (map[i - 1][j] = Empty)
+                    assert (map[i - 1][j + 1] = Empty)
+                    //delegate
+                    pushUp (i, j) map |> Option.get)
+            // .[
+            | Empty, BoxL ->
+                pushUp (i - 1, j + 1) map
+                |> Option.map (fun map ->
+                    assert (map[i - 1][j] = Empty)
+                    assert (map[i - 1][j + 1] = Empty)
+                    //delegate
+                    pushUp (i, j) map |> Option.get)
+            // ][
+            | BoxR, BoxL ->
+                pushUp (i - 1, j) map
+                |> Option.bind (fun map ->
+                    assert (map[i - 1][j - 1] = Empty)
+                    assert (map[i - 1][j] = Empty)
+                    pushUp (i - 1, j + 1) map)
+                |> Option.map (fun map ->
+                    assert (map[i - 1][j] = Empty)
+                    assert (map[i - 1][j + 1] = Empty)
+                    //delegate
+                    pushUp (i, j) map |> Option.get)
+            | c1, c2 -> failwithf $"%A{c1} %A{c2} !?"
+        | BoxR ->
+            //delegate
+            pushUp (i, j - 1) map
+        | c -> failwithf $"%A{c} !?"
+
+
+
+    let rec moveLeft (map: Cell2[][]) =
+        let ri, rj = findRobot map
+
+        match map[ri][rj - 1] with
+        | Wall -> map
+        | Empty ->
+            // #...@. -> #..@..
+            let newRow = map[ri] |> swap (rj - 1, rj)
+            map |> Array.updateAt ri newRow
+        | BoxR ->
+            map
+            |> pushLeft (ri, rj - 1)
+            |> Option.map (fun map ->
+                assert (map[ri][rj - 1] = Empty)
+                //delegate
+                moveLeft map)
+            |> Option.defaultWith (fun () -> map)
+        | c -> failwithf $"%A{c} !?"
+
+    let rec moveUp (map: Cell2[][]) =
+        let ri, rj = findRobot map
+
+        match map[ri - 1][rj] with
+        | Wall -> map
+        | Empty ->
+            let newRow1 = map[ri - 1] |> Array.updateAt rj Robot
+            let newRow2 = map[ri] |> Array.updateAt rj Empty
+            map |> Array.updateAt (ri - 1) newRow1 |> Array.updateAt ri newRow2
+        | BoxL
+        | BoxR ->
+            map
+            |> pushUp (ri - 1, rj)
+            |> Option.map (fun map ->
+                assert (map[ri - 1][rj] = Empty)
+                //delegate
+                moveUp map)
+            |> Option.defaultWith (fun () -> map)
+        | c -> failwithf $"%A{c} !?"
+
+    let moveRight (map: Cell2[][]) =
+        let reverse (map: Cell2[][]) =
+            map
+            |> Array.map (fun row ->
+                row
+                |> Array.rev
+                |> Array.map (function
+                    | BoxL -> BoxR
+                    | BoxR -> BoxL
+                    | c -> c))
+
+        map |> reverse |> moveLeft |> reverse
+
+    let moveDown (map: Cell2[][]) = map |> Array.rev |> moveUp |> Array.rev
+
+    let scaleUp (map: Cell[][]) =
+        map
+        |> Array.map (fun row ->
+            row
+            |> Array.collect (function
+                | Cell.Robot -> [| Robot; Empty |]
+                | Cell.Box -> [| BoxL; BoxR |]
+                | Cell.Wall -> [| Wall; Wall |]
+                | Cell.Empty -> [| Empty; Empty |]
+                | _ -> System.ArgumentOutOfRangeException() |> raise))
+
+    let part2 ((map, moves): (Cell[][] * Dir seq)) =
+        let map =
+            (scaleUp map, moves)
+            ||> Seq.fold (fun map dir ->
+                let mv =
+                    match dir with
+                    | Up -> moveUp
+                    | Left -> moveLeft
+                    | Down -> moveDown
+                    | Right -> moveRight
+
+                mv map)
+
+        List.allPairs [ 0 .. (map.Length - 1) ] [ 0 .. (map[0].Length - 1) ]
+        |> List.sumBy (fun (i, j) -> if map[i][j] = BoxL then i * 100 + j else 0)
 
 
 let parseMap (input: string) =
@@ -152,6 +340,7 @@ let main _ =
     let stopwatch = Stopwatch.StartNew()
 
     (map, moves) |> part1 |> printfn "Part 1: %d"
+    (map, moves) |> Part2.part2 |> printfn "Part 2: %d"
 
     stopwatch.Stop()
     printfn $"Elapsed time: %.4f{stopwatch.Elapsed.TotalSeconds} seconds"
