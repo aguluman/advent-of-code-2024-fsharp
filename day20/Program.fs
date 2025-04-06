ae96955d7937777176d2c9322af82cfa9e43dcaa
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2025-04-06 16:26:00 +0100

    refactor: improve code formatting and readability in BFS implementation

--- day20/Program.fs
+++ day20/Program.fs
@@ -58,45 +58,50 @@ let findIndex2D (grid: 'T[][]) (target: 'T) =
 /// <param name="startCol">Starting column index</param>
 /// <param name="maze">2D character array representing the maze</param>
 /// <returns>Dictionary mapping cell coordinates to their shortest distance from start</returns>
-let bfsFast (startRow, startCol) (maze: char[][]) = 
-    let queue = Queue<struct(int * int)>(maze.Length * maze[0].Length)
-    queue.Enqueue(struct(startRow, startCol))
-    
-    let distances = Dictionary<struct(int * int), int>(maze.Length * maze[0].Length)
-    distances[struct(startRow, startCol)] <- 0
-    
+let bfsFast (startRow, startCol) (maze: char[][]) =
+    let queue = Queue<struct (int * int)>(maze.Length * maze[0].Length)
+    queue.Enqueue(struct (startRow, startCol))
+
+    let distances = Dictionary<struct (int * int), int>(maze.Length * maze[0].Length)
+    distances[struct (startRow, startCol)] <- 0
+
     // Pre-calculate bounds
     let maxRow, maxCol = maze.Length - 1, maze[0].Length - 1
-    
+
     while queue.Count > 0 do
-        let struct(row, col) = queue.Dequeue()
-        let distance = distances[struct(row, col)]
+        let struct (row, col) = queue.Dequeue()
+        let distance = distances[struct (row, col)]
         let nextDistance = distance + 1
-        
+
         // Manually unrolled loop with bounds checking first
-        if row > 0 && maze[row-1].[col] <> '#' then
-            let nextPos = struct(row-1, col)
+        if row > 0 && maze[row - 1].[col] <> '#' then
+            let nextPos = struct (row - 1, col)
+
             if not (distances.ContainsKey(nextPos)) then
                 distances[nextPos] <- nextDistance
                 queue.Enqueue(nextPos)
-                
-        if col > 0 && maze[row].[col-1] <> '#' then
-            let nextPos = struct(row, col-1)
+
+        if col > 0 && maze[row].[col - 1] <> '#' then
+            let nextPos = struct (row, col - 1)
+
             if not (distances.ContainsKey(nextPos)) then
                 distances[nextPos] <- nextDistance
                 queue.Enqueue(nextPos)
-                
-        if row < maxRow && maze[row+1].[col] <> '#' then
-            let nextPos = struct(row+1, col)
+
+        if row < maxRow && maze[row + 1].[col] <> '#' then
+            let nextPos = struct (row + 1, col)
+
             if not (distances.ContainsKey(nextPos)) then
                 distances[nextPos] <- nextDistance
                 queue.Enqueue(nextPos)
-                
-        if col < maxCol && maze[row].[col+1] <> '#' then
-            let nextPos = struct(row, col+1)
+
+        if col < maxCol && maze[row].[col + 1] <> '#' then
+            let nextPos = struct (row, col + 1)
+
             if not (distances.ContainsKey(nextPos)) then
                 distances[nextPos] <- nextDistance
                 queue.Enqueue(nextPos)
+
     distances
 
 
@@ -123,55 +128,76 @@ let bfsFast (startRow, startCol) (maze: char[][]) =
 let part1 (maze: char[][]) =
     let startRow, startCol = findIndex2D maze 'S'
     let endRow, endCol = findIndex2D maze 'E'
-    
+
     let distancesFromStart = bfsFast (startRow, startCol) maze
-    let endPosition = struct(endRow, endCol)
-    
-    if not (distancesFromStart.ContainsKey(endPosition)) then []
+    let endPosition = struct (endRow, endCol)
+
+    if not (distancesFromStart.ContainsKey(endPosition)) then
+        []
     else
         let originalDistance = distancesFromStart[endPosition]
-        let visited = HashSet<struct(int * int)>()
-        let walls = HashSet<struct(int * int)>()
-        
+        let visited = HashSet<struct (int * int)>()
+        let walls = HashSet<struct (int * int)>()
+
         // Find walls along the path using BFS
-        let queue = Queue<struct(int * int)>()
+        let queue = Queue<struct (int * int)>()
         queue.Enqueue(endPosition)
         visited.Add(endPosition) |> ignore
-        
+
         while queue.Count > 0 do
-            let struct(row, col) = queue.Dequeue()
-            for deltaRow, deltaCol in [|(-1,0); (1,0); (0,-1); (0,1)|] do
+            let struct (row, col) = queue.Dequeue()
+
+            for deltaRow, deltaCol in [| (-1, 0); (1, 0); (0, -1); (0, 1) |] do
                 let neighborRow, neighborCol = row + deltaRow, col + deltaCol
-                if neighborRow >= 0 && neighborRow < maze.Length && neighborCol >= 0 && neighborCol < maze[0].Length then
-                    let neighborPos = struct(neighborRow, neighborCol)
+
+                if
+                    neighborRow >= 0
+                    && neighborRow < maze.Length
+                    && neighborCol >= 0
+                    && neighborCol < maze[0].Length
+                then
+                    let neighborPos = struct (neighborRow, neighborCol)
+
                     if maze[neighborRow].[neighborCol] = '#' then
                         walls.Add(neighborPos) |> ignore
-                    elif not (visited.Contains(neighborPos)) && distancesFromStart.ContainsKey(neighborPos) then
+                    elif
+                        not (visited.Contains(neighborPos))
+                        && distancesFromStart.ContainsKey(neighborPos)
+                    then
                         visited.Add(neighborPos) |> ignore
                         queue.Enqueue(neighborPos)
-        
+
         // Process walls in parallel with minimal copying
         let improvements = Dictionary<int, int>()
-        let lockObj = obj()
-        
+        let lockObj = obj ()
+
         // Fixed parallel processing block
-        Parallel.ForEach(walls, fun wallPos ->
-            let struct(wallRow, wallCol) = wallPos
-            let mazeWithRemovedWall = Array.map Array.copy maze
-            mazeWithRemovedWall[wallRow].[wallCol] <- '.'
-            
-            let newDistances = bfsFast (startRow, startCol) mazeWithRemovedWall
-            if newDistances.ContainsKey(endPosition) then
-                let improvement = originalDistance - newDistances[endPosition]
-                if improvement > 0 then
-                    lock lockObj (fun () ->
-                        improvements[improvement] <- 
-                            if improvements.ContainsKey(improvement) then 
-                                improvements[improvement] + 1 
-                            else 1)
-        ) |> ignore
-        
-        improvements |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Seq.toList |> List.sort
+        Parallel.ForEach(
+            walls,
+            fun wallPos ->
+                let struct (wallRow, wallCol) = wallPos
+                let mazeWithRemovedWall = Array.map Array.copy maze
+                mazeWithRemovedWall[wallRow].[wallCol] <- '.'
+
+                let newDistances = bfsFast (startRow, startCol) mazeWithRemovedWall
+
+                if newDistances.ContainsKey(endPosition) then
+                    let improvement = originalDistance - newDistances[endPosition]
+
+                    if improvement > 0 then
+                        lock lockObj (fun () ->
+                            improvements[improvement] <-
+                                if improvements.ContainsKey(improvement) then
+                                    improvements[improvement] + 1
+                                else
+                                    1)
+        )
+        |> ignore
+
+        improvements
+        |> Seq.map (fun kvp -> kvp.Key, kvp.Value)
+        |> Seq.toList
+        |> List.sort
 
 
 
@@ -197,39 +223,50 @@ let part1 (maze: char[][]) =
 ///
 /// <param name="maze">2D character array representing the maze</param>
 /// <returns>List of (inefficiency, count) tuples sorted by inefficiency value</returns>
-let part2 (maze: char[][]) = 
+let part2 (maze: char[][]) =
     let startRow, startCol = findIndex2D maze 'S'
     let distances = bfsFast (startRow, startCol) maze
-    
+
     let inefficiencies = Dictionary<int, int>()
     let reachablePoints = distances.Keys |> Seq.toArray
-    
+
     // Process in chunks for better CPU utilization
-    Parallel.For(0, reachablePoints.Length, fun i ->
-        let struct(row1, col1) = reachablePoints[i]
-        let distance1 = distances[struct(row1, col1)]
-        
-        let localInefficiencies = Dictionary<int, int>()
-        for j = i + 1 to reachablePoints.Length - 1 do
-            let struct(row2, col2) = reachablePoints[j]
-            let distance2 = distances[struct(row2, col2)]
-            
-            let manhattanDistance = abs(row1 - row2) + abs(col1 - col2)
-            if manhattanDistance <= 20 then
-                let inefficiency1 = distance2 - distance1 - manhattanDistance
-                if inefficiency1 >= 0 then
-                    localInefficiencies[inefficiency1] <- localInefficiencies.GetValueOrDefault(inefficiency1) + 1
-                
-                let inefficiency2 = distance1 - distance2 - manhattanDistance
-                if inefficiency2 >= 0 then
-                    localInefficiencies[inefficiency2] <- localInefficiencies.GetValueOrDefault(inefficiency2) + 1
-                    
-        lock inefficiencies (fun () ->
-            for KeyValue(inefficiency, count) in localInefficiencies do
-                inefficiencies[inefficiency] <- inefficiencies.GetValueOrDefault(inefficiency) + count)
-    ) |> ignore
-    
-    inefficiencies |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Seq.toList |> List.sort
+    Parallel.For(
+        0,
+        reachablePoints.Length,
+        fun i ->
+            let struct (row1, col1) = reachablePoints[i]
+            let distance1 = distances[struct (row1, col1)]
+
+            let localInefficiencies = Dictionary<int, int>()
+
+            for j = i + 1 to reachablePoints.Length - 1 do
+                let struct (row2, col2) = reachablePoints[j]
+                let distance2 = distances[struct (row2, col2)]
+
+                let manhattanDistance = abs (row1 - row2) + abs (col1 - col2)
+
+                if manhattanDistance <= 20 then
+                    let inefficiency1 = distance2 - distance1 - manhattanDistance
+
+                    if inefficiency1 >= 0 then
+                        localInefficiencies[inefficiency1] <- localInefficiencies.GetValueOrDefault(inefficiency1) + 1
+
+                    let inefficiency2 = distance1 - distance2 - manhattanDistance
+
+                    if inefficiency2 >= 0 then
+                        localInefficiencies[inefficiency2] <- localInefficiencies.GetValueOrDefault(inefficiency2) + 1
+
+            lock inefficiencies (fun () ->
+                for KeyValue(inefficiency, count) in localInefficiencies do
+                    inefficiencies[inefficiency] <- inefficiencies.GetValueOrDefault(inefficiency) + count)
+    )
+    |> ignore
+
+    inefficiencies
+    |> Seq.map (fun kvp -> kvp.Key, kvp.Value)
+    |> Seq.toList
+    |> List.sort
 
 
 
@@ -314,8 +351,8 @@ module Example =
                   (72, 22)
                   (74, 4)
                   (76, 3) ])
-            
-            
+
+
 
 /// <summary>Main entry point for the program</summary>
 [<EntryPoint>]
