87baf9209679edc7e784915ca55fc4c72d37611e
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2025-04-07 18:56:52 +0100

    Add solution and implementation for day21 challenge
    Introduced `day21` project with its code and added it to the solution. The implementation includes functions to calculate minimum cost paths and solve parts 1 and 2 of the challenge, as well as tests using NUnit.

---
+++ day21/Program.fs
@@ -0,0 +1,171 @@
+﻿module day21
+
+open System.Diagnostics
+open NUnit.Framework
+open FsUnit
+
+
+let numPos button =
+    match button with
+    | '7' -> (0, 0)
+    | '8' -> (0, 1)
+    | '9' -> (0, 2)
+    | '4' -> (1, 0)
+    | '5' -> (1, 1)
+    | '6' -> (1, 2)
+    | '1' -> (2, 0)
+    | '2' -> (2, 1)
+    | '3' -> (2, 2)
+    | '0' -> (3, 1)
+    | 'A' -> (3, 2)
+    | c -> failwithf $"%c{c} !?"
+
+
+
+let dirPos button =
+    match button with
+    | '^' -> (0, 1)
+    | 'A' -> (0, 2)
+    | '<' -> (1, 0)
+    | 'v' -> (1, 1)
+    | '>' -> (1, 2)
+    | c -> failwithf $"%c{c} !?"
+
+
+
+let numRoute num1 num2 =
+    let xi, xj = numPos num1
+    let yi, yj = numPos num2
+    let di = List.init (abs (xi - yi)) (fun _ -> if xi < yi then 'v' else '^')
+    let dj = List.init (abs (xj - yj)) (fun _ -> if xj < yj then '>' else '<')
+    let tate = di @ dj @ [ 'A' ]
+    let yoko = dj @ di @ [ 'A' ]
+
+    let bottom = [ '0'; 'A' ]
+    let left = [ '7'; '4'; '1' ]
+
+    match num1, num2 with
+    | num1, num2 when List.contains (num1, num2) (List.allPairs bottom left) -> [ tate ]
+    | num1, num2 when List.contains (num1, num2) (List.allPairs left bottom) -> [ yoko ]
+    | _ -> List.distinct [ tate; yoko ]
+
+
+let dirRoute dir1 dir2 =
+    let x1, xj = dirPos dir1
+    let y1, yj = dirPos dir2
+    let di = List.init (abs (x1 - y1)) (fun _ -> if x1 < y1 then 'v' else '^')
+    let dj = List.init (abs (xj - yj)) (fun _ -> if xj < yj then '>' else '<')
+    let tate = di @ dj @ [ 'A' ]
+    let yoko = dj @ di @ [ 'A' ]
+
+    let top = [ '^'; 'A' ]
+    let left = [ '<' ]
+
+
+    match dir1, dir2 with
+    | dir1, dir2 when List.contains (dir1, dir2) (List.allPairs top left) -> [ tate ]
+    | dir1, dir2 when List.contains (dir1, dir2) (List.allPairs left top) -> [ yoko ]
+    | _ -> List.distinct [ tate; yoko ]
+
+
+let rec minCostPath level b1 b2 =
+    if level = 0 then
+        [ b2 ]
+    else
+        let numKeypad = '0' <= b1 && b1 <= '9' || '0' <= b2 && b2 <= '9'
+        let route = if numKeypad then numRoute b1 b2 else dirRoute b1 b2
+
+        route
+        |> List.map (fun route ->
+            'A' :: route
+            |> List.pairwise
+            |> List.collect (fun (x, y) -> minCostPath (level - 1) x y))
+        |> List.minBy List.length
+
+
+let memo = System.Collections.Generic.Dictionary()
+
+
+let rec minCost level b1 b2 =
+    let key = (level, b1, b2)
+
+    match memo.TryGetValue(key) with
+    | true, value -> value
+    | false, _ ->
+        let value =
+            if level = 0 then
+                1L
+            else
+                let numKeypad = '0' <= b1 && b1 <= '9' || '0' <= b2 && b2 <= '9'
+                let route = if numKeypad then numRoute b1 b2 else dirRoute b1 b2
+
+                route
+                |> List.map (fun route ->
+                    'A' :: route
+                    |> List.pairwise
+                    |> List.sumBy (fun (x, y) -> minCost (level - 1) x y))
+                |> List.min
+
+        memo.Add(key, value)
+        value
+
+
+let part1 (codes: string seq) =
+    codes
+    |> Seq.sumBy (fun code ->
+        let cost =
+            'A' :: List.ofSeq code
+            |> List.pairwise
+            |> List.collect (fun (x, y) -> minCostPath (2 + 1) x y)
+            |> List.length
+            |> int64
+
+        let num = code.TrimEnd 'A' |> int64
+        cost * num)
+
+
+let part2 (codes: string seq) =
+    codes
+    |> Seq.sumBy (fun code ->
+        let cost =
+            'A' :: List.ofSeq code
+            |> List.pairwise
+            |> List.sumBy (fun (x, y) -> minCost (25 + 1) x y)
+
+        let num = code.TrimEnd 'A' |> int64
+        cost * num)
+
+
+let parse (input: string) = input.Split "\n" |> Array.map _.Trim()
+
+
+
+module Example =
+    let input =
+        "029A
+980A
+179A
+456A
+379A"
+
+    [<Test>]
+    let testPart1 () =
+        parse input |> part1 |> should equal 126384L
+
+
+[<EntryPoint>]
+let main _ =
+    let input = stdin.ReadToEnd().TrimEnd()
+    let codes = parse input
+
+    let stopwatch = Stopwatch()
+    stopwatch.Start()
+
+    codes |> part1 |> printfn "Part 1: %d"
+    codes |> part2 |> printfn "Part 2: %d"
+
+    stopwatch.Stop()
+    printfn $"Elapsed time: %.4f{stopwatch.Elapsed.TotalSeconds} seconds"
+
+
+    0
