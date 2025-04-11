a86d4fa26fd9084a68c42566bd87882a00b5a780
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2025-04-11 17:06:31 +0100

    Elapsed time: 0.1427 seconds Add Day 25 project and implementation
    Introduced the Day 25 project with F# files and updated the solution configuration (AdventOfCode2024.sln). Added the solution for Part 1, including parsing logic, example test cases, and performance measurement. Integrated dependencies such as NUnit and FsUnit for testing.

---
+++ day25/Program.fs
@@ -0,0 +1,101 @@
+﻿module day25
+
+open System.Diagnostics
+open NUnit.Framework
+open FsUnit
+
+
+let part1 (schematics: char[][] list) =
+    let h, w = schematics[0].Length, (schematics[0][0]).Length
+
+    let locks =
+        schematics
+        |> List.filter (fun s -> s[0] |> Array.forall ((=) '#'))
+        |> List.map (fun s ->
+            [ 0 .. (w - 1) ]
+            |> List.map (fun j -> [ 0 .. (h - 1) ] |> List.sumBy (fun i -> if s[i][j] = '.' then 0 else 1)))
+
+    let keys =
+        schematics
+        |> List.filter (fun s -> s[0] |> Array.forall ((=) '.'))
+        |> List.map (fun s ->
+            [ 0 .. (w - 1) ]
+            |> List.map (fun j -> [ 0 .. (h - 1) ] |> List.sumBy (fun i -> if s[i][j] = '.' then 0 else 1)))
+
+    List.allPairs locks keys
+    |> List.filter (fun (lock, key) -> List.zip lock key |> List.forall (fun (lock, key) -> lock + key <= h))
+    |> List.length
+
+
+let parse (input: string) =
+    input.Split([| "\n\n"; "\r\n\r\n" |], System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun section ->
+        section.Split([| "\n"; "\r\n" |], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun line -> line.TrimEnd().ToCharArray()))
+    |> Array.toList
+
+
+
+module Example =
+    let input =
+        "#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"
+
+    [<Test>]
+    let testPart1 () = parse input |> part1 |> should equal 3
+
+
+[<EntryPoint>]
+let main _ =
+    let input = stdin.ReadToEnd().TrimEnd()
+    let schematics = parse input
+
+    let stopwatch = Stopwatch()
+    stopwatch.Start()
+
+    schematics |> part1 |> printfn "Part 1: %d"
+
+    stopwatch.Stop()
+
+    printfn $"Elapsed time: %.4f{stopwatch.Elapsed.TotalSeconds} seconds"
+
+
+
+    0
